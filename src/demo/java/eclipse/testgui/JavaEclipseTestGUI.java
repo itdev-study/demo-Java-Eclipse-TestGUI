b583683ceee2aa5ccb812c3c877cda15d28da15e
o.levitskiy <o.levitskiy@GR-MS-IT03.ipmatika.local>
2018-04-05 17:10:48 +0300

    Test Alfa 0.1

---
+++ src/demo/java/eclipse/testgui/JavaEclipseTestGUI.java
@@ -0,0 +1,43 @@
+package demo.java.eclipse.testgui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class JavaEclipseTestGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JavaEclipseTestGUI window = new JavaEclipseTestGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JavaEclipseTestGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
